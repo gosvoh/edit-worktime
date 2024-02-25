9a08630983fa9ed1c7c81b6606d532c9fd03c6b4
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-02-26 00:57:33 +0300

    Refactor Home component to include authentication and dynamic imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Editor from "@/components/Editor";
+import { validateRequest } from "@/lib/auth";
+import { readFile } from "fs/promises";
+import dynamic from "next/dynamic";
+import { redirect } from "next/navigation";
+const Editor = dynamic(
+  async () => (await import("@/components/Editor")).default,
+  { ssr: false }
+);
 
-export default function Home() {
+export default async function Home() {
+  const { user } = await validateRequest();
+  if (!user) return redirect("/login");
+
   return (
     <main>
-      <Editor />
+      <Editor
+        elements={await readFile("elements.json", "utf-8")
+          .then(JSON.parse)
+          .catch(() => [])}
+        library={await readFile("library.json", "utf-8")
+          .then(JSON.parse)
+          .catch(() => [])}
+      />
     </main>
   );
 }
